955b34c6667df91569f8d4039829bb939a6dd5f2
Bigtree <451805261@qq.com>
2015-10-28 15:03:50 +0800

    initial commit

---
+++ UIImage+FNAppImage.h
@@ -0,0 +1,16 @@
+//
+//  UIImage+FNSystemImage.h
+//  FNMarket
+//
+//  Created by FNNishipu on 10/26/15.
+//  Copyright © 2015 cn.com.feiniu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (FNAppImage)
+
++ (UIImage *)fn_lanuchImage;
++ (UIImage *)fn_appIconImage;
+
+@end
